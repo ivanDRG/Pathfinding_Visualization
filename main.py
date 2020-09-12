970984f5e646fde29720720a23b8baf6a824f6e2
IvanDRG <delriogestidoivan@gmail.com>
2020-09-12 00:09:18 +0000

    final config and cleaning code

--- main.py
+++ main.py
@@ -187,15 +187,11 @@ def main(win, width):
     end = None
 
     run = True
-    started = False
     while run:
         draw(win, grid, ROWS, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
-            if started:
-                continue
 
             if pygame.mouse.get_pressed()[0]: #LEFT
                 pos = pygame.mouse.get_pos()
@@ -224,7 +220,7 @@ def main(win, width):
                     end = None  
                 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end:
                     for row in grid:
                         for node in row:
                             node.update_neighbors(grid)
